2c91330b18adc9f29d70a46701cd4bedfa3b61fb
Daniel-G-BP <lalooos@seznam.cz>
2025-04-16 13:14:58 +0200

    úprava config-example.php pro docker

--- includes/config-example.php
+++ includes/config-example.php
@@ -1,12 +1,21 @@
 <?php 
 //vyplnit a zmenit soubor na config.php
-//login do stagu
+
+//login do STAGu
 $username = "prihlasovaciJmeno";
 $password = "heslo";
 
-// login DB
+// login DB bez dockeru
 
-$servername = "localhost";
+//$servername = "localhost"; //pokud bez dockeru, toto tento řádek odkomentovat 
 $user = "root";
 $passwordDB = "hesloDoDatabaze";
 $database = "atpu";
+
+//pro docker
+
+$host = 'db';             
+$db   = 'atpu';           
+$user = 'user';           
+$pass = 'password';       
+$charset = 'utf8mb4';
